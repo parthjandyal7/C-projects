9587c1306d4d1f837905f1a661ea2bacb0b6f507
parthjandyal7 <2025a1r068@mietjammu.in>
2025-09-23 15:54:56 +0530

    Add files via upload
    It calculates distance between two points in a plane by entering coordinates

---
+++ euclidean_distance.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, x2, y1, y2;
+    float distance;
+
+    printf("Enter x1 and y1 : ");
+    scanf("%d %d", &x1, &y1);
+
+    printf("Enter x2 and y2 : ");
+    scanf("%d %d", &x2, &y2);
+
+    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("Euclidean distance = %f\n", distance);
+
+    return 0;
+}
